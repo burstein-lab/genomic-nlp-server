f02f925782feabe0b6721b576d00330c059a0e8e
notofir <ofirarias@gmail.com>
2022-08-23 11:50:37 +0300

    Update ControlCard.vue (#79)

--- web/components/ControlCard.vue
+++ web/components/ControlCard.vue
@@ -7,24 +7,33 @@
           color="primary"
           label="Show Map"
           hide-details
-        ></v-switch>
+        />
+        <v-select
+          v-model="searchMode"
+          :items="searchModes"
+          label="Search Mode"
+        />
         <search
+          v-if="searchMode === 'Space'"
           @select="(e: string[]) => onSelect('space', e)"
           label="Space"
           type="space"
         />
         <search
+          v-if="searchMode === 'Label'"
           @select="(e: string[]) => onSelect('label', e)"
           label="Label"
           type="label"
         />
         <search
+          v-if="searchMode === 'KO / Hypo'"
           @select="(e: string[]) => onSelect('word', e)"
           label="KO / Hypo"
           type="word"
           multiple
         />
         <search
+          v-if="searchMode === 'Neighbors'"
           @select="(e: string[]) => onSelect('neighbors', e)"
           label="Neighbors"
           type="word"
@@ -45,6 +54,8 @@ export default {
     },
   },
   data: () => ({
+    searchMode: null,
+    searchModes: ["Space", "Label", "KO / Hypo", "Neighbors"],
     shouldShowMap: useShouldShowMap(),
   }),
   methods: {
