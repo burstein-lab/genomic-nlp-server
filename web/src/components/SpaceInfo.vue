c91546d2887a4572ef0dab3e963e425fdb15a41c
notofir <ofirarias@gmail.com>
2022-12-27 15:31:05 +0200

    #114 (#118)

---
+++ web/src/components/SpaceInfo.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Word: {{ space.value.word }} <br />
+    KO: {{ space.value.ko }} <br />
+    Label: {{ space.value.label }} <br />
+    Product: {{ space.value.product }} <br />
+    Gene Name: {{ space.value.gene_name }} <br />
+    Significant: {{ space.value.significant }} <br />
+    Predicted Class: {{ space.value.predicted_class }} <br />
+  </div>
+</template>
+
+<script lang="ts">
+import { Space } from "../composables/spaces";
+
+export default {
+  name: "SpaceInfo",
+  components: {},
+  props: {
+    space: {
+      type: Object,
+    },
+  },
+};
+</script>
