81717b7541bd2d7b216e38c0bfd30f5345050a5d
notofir <ofirarias@gmail.com>
2023-11-18 12:20:41 -0500

    Resolves #245

---
+++ web/src/components/Legend.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card class="mx-auto" max-width="500">
+    <v-list dense>
+      <v-list-item-group v-model="selectedItem" color="primary">
+        <v-list-item v-for="(item, i) in items" :key="i">
+          <v-list-item-icon>
+            <v-icon v-text="item.icon"></v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Legend",
+  data: () => {
+    return {
+      selectedItem: 0,
+      items: [
+        { text: "Test 1", icon: "mdi-star" },
+        { text: "Test 2", icon: "mdi-star" },
+        { text: "Test 3", icon: "mdi-star" },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
