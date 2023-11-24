643bf76692b09c7343afa4eb403ba14ddc495e05
notofir <ofirarias@gmail.com>
2023-11-24 16:34:48 -0500

    Update Legend.vue

--- web/src/components/Legend.vue
+++ web/src/components/Legend.vue
@@ -1,37 +1,38 @@
 <template>
-  <v-overlay
-    :contained="true"
-    :persistent="true"
-    location-strategy="connected"
-    location="start bottom"
-    origin="top start"
-    width="400"
-    :no-click-animation="true"
-  >
-    <template #activator="{ isActive, props }">
-      <v-btn v-bind="props" color="info" size="small">
-        Legend <v-icon>mdi-chevron-{{ isActive ? "down" : "up" }}</v-icon>
-      </v-btn>
-    </template>
-
-    <v-card style="opacity: 0.9">
-      <v-list :lines="false" density="compact">
-        <v-list-item
-          v-for="item in items"
-          :key="item.color"
-          :color="selectedItem === item.color ? 'info' : undefined"
-          size="small"
-          active-color="info"
-          v-model="selectedItem"
-        >
-          <template v-slot:prepend>
-            <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
-          </template>
-          <v-list-item-title v-text="item.text"></v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-card>
-  </v-overlay>
+  <div>
+    <v-btn @click="isActive = !isActive" color="info" id="legend" size="small">
+      Legend <v-icon>mdi-chevron-{{ isActive ? "down" : "up" }}</v-icon>
+    </v-btn>
+    <v-overlay
+      attach="#legend"
+      :v-model="isActive"
+      :contained="true"
+      :persistent="true"
+      location-strategy="connected"
+      location="start bottom"
+      origin="top start"
+      width="400"
+      :no-click-animation="true"
+    >
+      <v-card style="opacity: 0.9">
+        <v-list :lines="false" density="compact">
+          <v-list-item
+            v-for="item in items"
+            :key="item.color"
+            :color="selectedItem === item.color ? 'info' : undefined"
+            size="small"
+            active-color="info"
+            v-model="selectedItem"
+          >
+            <template v-slot:prepend>
+              <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
+            </template>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-overlay>
+  </div>
 </template>
 
 <script lang="ts">
