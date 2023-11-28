9aaca807e2fd33dd0c0820f1e7ec9c5a9b6aefcb
notofir <ofirarias@gmail.com>
2023-11-28 10:48:43 -0500

    Update Legend.vue

--- web/src/components/Legend.vue
+++ web/src/components/Legend.vue
@@ -5,17 +5,14 @@
     <v-tooltip activator="parent" location="top" id="legend-tooltip">
       <v-card style="opacity: 0.9">
         <v-list :lines="false" density="compact">
-          <v-list-item
-            v-for="item in items"
-            :key="item.color"
-            color="success"
-            :input-value="true"
-          >
-            <template v-slot:prepend>
-              <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
-            </template>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item>
+          <v-list-item-group v-model="selectedItem" color="info">
+            <v-list-item v-for="item in items" :key="item.color">
+              <template v-slot:prepend>
+                <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
+              </template>
+              <v-list-item-title v-text="item.text"></v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
         </v-list>
       </v-card>
     </v-tooltip>
