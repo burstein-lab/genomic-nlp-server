8999523290c8a0bb8ac037edca61ceebdc1d1568
notofir <ofirarias@gmail.com>
2023-11-28 10:51:39 -0500

    Update Legend.vue

--- web/src/components/Legend.vue
+++ web/src/components/Legend.vue
@@ -5,7 +5,7 @@
     <v-tooltip activator="parent" location="top" id="legend-tooltip">
       <v-card style="opacity: 0.9">
         <v-list :lines="false" density="compact">
-          <v-list-item-group v-model="selectedItem" color="info">
+          <v-list-item-group v-model="selectedItem" color="success">
             <v-list-item v-for="(item, i) in items" :key="i">
               <template v-slot:prepend>
                 <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
