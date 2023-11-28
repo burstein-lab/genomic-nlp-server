cfbc068fdfc7294c3b8ea203cc26331e9fe4105e
notofir <ofirarias@gmail.com>
2023-11-28 10:45:35 -0500

    Update Legend.vue

--- web/src/components/Legend.vue
+++ web/src/components/Legend.vue
@@ -5,7 +5,12 @@
     <v-tooltip activator="parent" location="top" id="legend-tooltip">
       <v-card style="opacity: 0.9">
         <v-list :lines="false" density="compact">
-          <v-list-item v-for="item in items" :key="item.color" color="success">
+          <v-list-item
+            v-for="item in items"
+            :key="item.color"
+            color="success"
+            :input-value="true"
+          >
             <template v-slot:prepend>
               <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
             </template>
