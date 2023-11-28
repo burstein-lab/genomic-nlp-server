526c06339b3225842f9e367f94628064a72e801c
notofir <ofirarias@gmail.com>
2023-11-28 10:43:52 -0500

    Resolves #298

--- web/src/components/Legend.vue
+++ web/src/components/Legend.vue
@@ -5,7 +5,7 @@
     <v-tooltip activator="parent" location="top" id="legend-tooltip">
       <v-card style="opacity: 0.9">
         <v-list :lines="false" density="compact">
-          <v-list-item v-for="item in items" :key="item.color">
+          <v-list-item v-for="item in items" :key="item.color" color="success">
             <template v-slot:prepend>
               <v-icon :color="item.color" class="me-2">mdi-circle</v-icon>
             </template>
