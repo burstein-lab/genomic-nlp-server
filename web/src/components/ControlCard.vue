d71ce8cd8aa4995bdb6e41c35712c35ae55f5238
notofir <ofirarias@gmail.com>
2023-12-10 11:21:07 -0500

    Resolves #231 (#316)

--- web/src/components/ControlCard.vue
+++ web/src/components/ControlCard.vue
@@ -39,6 +39,7 @@
         <v-select
           color="info"
           v-model="queryParams.searchMode"
+          :menu-props="{ maxHeight: '500' }"
           @update:modelValue="
             (v: string) => {
               queryParams.searchValue = '';
